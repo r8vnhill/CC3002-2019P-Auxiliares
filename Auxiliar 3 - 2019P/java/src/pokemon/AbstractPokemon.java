8d5a1b55e66138b28b7beb3cc25ec3005c1a45a3
islaterm <ignacio.slater@ug.uchile.cl>
2019-08-25 21:45:56 -0400

    Se incluyen los Pokémon (puede contener errores)

--- Auxiliar 3 - 2019P/java/src/pokemon/AbstractPokemon.java
+++ Auxiliar 3 - 2019P/java/src/pokemon/AbstractPokemon.java
@@ -1,23 +1,102 @@
-package pokemon;
+package cc3002.pokemon;
 
-import java.util.ArrayList;
-import pokemon.attack.IAttack;
+import cc3002.pokemon.fire.FireAttack;
+import cc3002.pokemon.grass.GrassAttack;
+import cc3002.pokemon.normal.NormalAttack;
+import cc3002.pokemon.water.WaterAttack;
+import java.util.List;
 
 /**
+ * Abstract class that represents a generic Pokémon. This class contains the necessary methods to
+ * attack and receive damage, and definitions to get the properties of each Pokémon, like its name
+ * and hp.
+ *
  * @author Ignacio Slater Muñoz
- * @since
  */
 public abstract class AbstractPokemon implements IPokemon {
+
   private String name;
   private int hp;
-  private int damage;
+  private List<IAttack> attackList;
+  private IAttack selectedAttack;
 
-  public AbstractPokemon(final String name, final int hp, final ArrayList<IAttack> attacks) {
+  /**
+   * Creates a new Pokémon.
+   *
+   * @param name
+   *     Pokémon's name.
+   * @param hp
+   *     Pokémon's hit points.
+   * @param attackList
+   *     Pokémon's attacks.
+   */
+  protected AbstractPokemon(String name, int hp, List<IAttack> attackList) {
     this.name = name;
     this.hp = hp;
-    this.damage = attacks;
+    this.attackList = attackList;
   }
 
+  @Override
+  public void selectAttack(int index) {
+    selectedAttack = attackList.get(index);
+  }
+
+  @Override
+  public void attack(IPokemon other) {
+    selectedAttack.attack(other);
+  }
+
+  @Override
+  public void receiveWaterAttack(WaterAttack attack) {
+    receiveAttack(attack);
+  }
+
+  /**
+   * Receives an attack.
+   *
+   * @param attack
+   *     Received attack.
+   */
+  protected void receiveAttack(IAttack attack) {
+    this.hp -= attack.getBaseDamage();
+  }
+
+  @Override
+  public void receiveGrassAttack(GrassAttack attack) {
+    receiveAttack(attack);
+  }
+
+  @Override
+  public void receiveFireAttack(FireAttack attack) {
+    receiveAttack(attack);
+  }
+
+  @Override
+  public void receiveNormalAttack(NormalAttack attack) {
+    receiveAttack(attack);
+  }
+
+  /**
+   * Receives an attack to which this Pokémon is weak.
+   *
+   * @param attack
+   *     Received attack.
+   */
+  protected void receiveWeaknessAttack(IAttack attack) {
+    this.hp -= attack.getBaseDamage() * 2;
+  }
+
+  /**
+   * Receives an attack to which this Pokémon is resistant.
+   *
+   * @param attack
+   *     Received attack.
+   */
+  protected void receiveResistantAttack(IAttack attack) {
+    this.hp -= attack.getBaseDamage() - 20;
+  }
+
+  //region Properties
   @Override
   public String getName() {
     return name;
@@ -29,12 +108,13 @@ public abstract class AbstractPokemon implements IPokemon {
   }
 
   @Override
-  public int getDamage() {
-    return damage;
+  public List<IAttack> getAttacks() {
+    return attackList;
   }
 
   @Override
-  public void selectAttack(int index) {
-
+  public IAttack getSelectedAttack() {
+    return selectedAttack;
   }
+  //endregion
 }
--- Auxiliar 3 - 2019P/java/src/pokemon/IPokemon.java
+++ Auxiliar 3 - 2019P/java/src/pokemon/IPokemon.java
@@ -1,28 +1,88 @@
 package pokemon;
 
 import java.util.List;
-import pokemon.attack.GrassAttack;
-import pokemon.attack.IAttack;
+import pokemon.fire.FireAttack;
+import pokemon.grass.GrassAttack;
+import pokemon.normal.NormalAttack;
+import pokemon.water.WaterAttack;
 
 /**
+ * A representation of a Pokémon that can battle others.
+ *
  * @author Ignacio Slater Muñoz
- * @since
+ * @version 1.2b1
+ * @since 1.2b1
  */
 public interface IPokemon {
 
+  //region Properties
+
+  /**
+   * @return Pokémon's name.
+   */
   String getName();
 
+  /**
+   * @return Pokémon's hit points
+   */
   int getHP();
 
-  int getDamage();
+  /**
+   * @return List with all the Pokémon attacks.
+   */
+  List<cc3002.pokemon.IAttack> getAttacks();
 
-  void attack(IPokemon pokemon);
+  /**
+   * @return The current selected attack.
+   */
+  cc3002.pokemon.IAttack getSelectedAttack();
+  //endregion
 
+  /**
+   * Attacks another Pokémon.
+   *
+   * @param other
+   *     Target of the attack.
+   */
+  void attack(IPokemon other);
+
+  /**
+   * Selects an attack.
+   *
+   * @param index
+   *     Index of the attack to be selected.
+   */
   void selectAttack(int index);
 
-  IAttack getSelectedAttack();
+  /**
+   * Receives damage from a water attack.
+   *
+   * @param attack
+   *     Received attack.
+   */
+  void receiveWaterAttack(WaterAttack attack);
 
-  List<IAttack> getAttacks();
+  /**
+   * Receives damage from a grass attack.
+   *
+   * @param attack
+   *     Received attack.
+   */
+  void receiveGrassAttack(GrassAttack attack);
 
-  void receiveGrassAttack(GrassAttack grassAttack);
+  /**
+   * Receives damage from a fire attack.
+   *
+   * @param attack
+   *     Received attack.
+   */
+  void receiveFireAttack(FireAttack attack);
+
+  /**
+   * Receives damage from a normal attack.
+   *
+   * @param attack
+   *     Received attack.
+   */
+  void receiveNormalAttack(NormalAttack attack);
 }
