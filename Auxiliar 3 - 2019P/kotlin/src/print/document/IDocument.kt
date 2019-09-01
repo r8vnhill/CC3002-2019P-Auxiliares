66ace4363f2902cf30d2fb783af8455f344af072
islaterm <ignacio.slater@ug.uchile.cl>
2019-09-01 11:33:22 -0400

    Added PDFDocument v1.2b2

--- Auxiliar 3 - 2019P/kotlin/src/print/document/IDocument.kt
+++ Auxiliar 3 - 2019P/kotlin/src/print/document/IDocument.kt
@@ -6,7 +6,8 @@ import print.printer.IPrinter
  * Common interface for document classes.
  *
  * @author Ignacio Slater Muñoz
- * @since 1.0b1
+ * @version 1.2b1
+ * @since 1.2b1
  */
 interface IDocument {
 
